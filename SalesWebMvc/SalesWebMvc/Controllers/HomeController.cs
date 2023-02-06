93b337c9c19ae6e2ff0ba477a89a73da31959dff
Alessandro Dias <adpdias@gmail.com>
2023-02-06 20:22:02 -0300

    First controller and Razor pages tests

--- SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web Site Course.";
 
             return View();
         }
